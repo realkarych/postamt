497658d617bb11df037c05e45f6fcd39217310f8
Andrey Karchevsky <karych@karych.ru>
2024-02-20 21:26:27 +0300

    Pump dependencies

--- app/entities/base.py
+++ app/entities/base.py
@@ -3,14 +3,17 @@ from pydantic import BaseModel
 
 class ModelWithDBMixin(BaseModel):
     """Base model with database mixin"""
+
     pass
 
 
 class DecodedModel(ModelWithDBMixin):
     """Model with database mixin for decoded data"""
+
     pass
 
 
 class EncodedModel(ModelWithDBMixin):
     """Model with database mixin for encoded data"""
+
     pass
