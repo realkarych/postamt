4f7f2e2bc511d42ee3f4620969766bffa77e1303
Andrey Karchevsky <karych@karych.ru>
2024-02-20 21:22:57 +0300

    Add stub entities' Base classes

---
+++ app/entities/base.py
@@ -0,0 +1,16 @@
+from pydantic import BaseModel
+
+
+class ModelWithDBMixin(BaseModel):
+    """Base model with database mixin"""
+    pass
+
+
+class DecodedModel(ModelWithDBMixin):
+    """Model with database mixin for decoded data"""
+    pass
+
+
+class EncodedModel(ModelWithDBMixin):
+    """Model with database mixin for encoded data"""
+    pass
