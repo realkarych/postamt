fe603de7037145e0f3e4bfdfc09d75707424ada0
Andrey Karchevsky <karych@karych.ru>
2024-02-27 10:15:16 +0300

    Representation dunder in models

--- app/models/email.py
+++ app/models/email.py
@@ -13,6 +13,10 @@ class EmailBox(Base):
     last_handled_email_id = Column(BigInteger, nullable=False, default=0)
     is_active = Column(Boolean, default=True)
 
+    def __repr__(self) -> str:
+        return f"Emailbox: {self.id} | Owner ID: {self.owner_id}, Forum ID: {self.forum_id}, " \
+               f"Last handled email ID: {self.last_handled_email_id}, Is active: {self.is_active}"
+
 
 class EmailAuthData(Base):
     """Implements model for email auth datas"""
@@ -23,3 +27,7 @@ class EmailAuthData(Base):
     email_server_id = Column(LargeBinary, nullable=False)
     email_address = Column(LargeBinary, nullable=False)
     email_password = Column(LargeBinary, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"EmailAuthData for {self.emailbox_id} | Email server ID: {self.email_server_id}, " \
+               f"Email address: {self.email_address}, Email password: {self.email_password}"
--- app/models/topic.py
+++ app/models/topic.py
@@ -10,3 +10,6 @@ class Topic(Base):
     forum_id = Column(BigInteger, primary_key=True, nullable=False)
     topic_id = Column(BigInteger, nullable=False)
     topic_title = Column(LargeBinary, nullable=False)
+
+    def __repr__(self) -> str:
+        return f"Topic: {self.topic_title} | Topic ID: {self.topic_id} | Forum ID: {self.forum_id}"
